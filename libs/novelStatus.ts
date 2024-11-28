38c26dac6a1dc510bdff751220c1cdb969e02c26
MartialGodAsura <103320562+MartialGodAsura@users.noreply.github.com>
2024-11-28 06:13:06 -0600

    Delete libs directory

--- libs/novelStatus.ts
+++
@@ -1,9 +0,0 @@
-export const NovelStatus = {
-  Unknown: 'Unknown',
-  Ongoing: 'Ongoing',
-  Completed: 'Completed',
-  Licensed: 'Licensed',
-  PublishingFinished: 'Publishing Finished',
-  Cancelled: 'Cancelled',
-  OnHiatus: 'On Hiatus',
-} as const;
